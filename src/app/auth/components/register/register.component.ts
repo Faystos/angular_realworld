d9f8fdae322fdce620fbc06e342badf93cece51d
Vladimir <fayst_@bk.ru>
2021-06-19 21:36:28 +0300

    создает селекторы для модуля авторизации.

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,7 +1,9 @@
-import {Component, OnInit} from "@angular/core";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {Store} from "@ngrx/store";
-import {registerAction} from "../../store/actions/register.action";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { select, Store } from "@ngrx/store";
+import { Observable } from "rxjs";
+import { registerAction } from "../../store/actions/register.action";
+import { isSubmittingSelector } from "../../store/selectors";
 
 @Component({
   selector: 'rw-register',
@@ -11,6 +13,7 @@ import {registerAction} from "../../store/actions/register.action";
 
 export class RegisterComponent implements OnInit{
   form!: FormGroup;
+  isSubmitting$!: Observable<boolean>
 
   constructor(
     private fb: FormBuilder,
@@ -18,6 +21,7 @@ export class RegisterComponent implements OnInit{
 
   ngOnInit(): void {
     this.initializeForm();
+    this.initializeValues();
   }
 
   initializeForm = (): void => {
@@ -26,6 +30,10 @@ export class RegisterComponent implements OnInit{
       email: '',
       password: ''
     });
+  }
+
+  initializeValues = (): void => {
+    this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector));
   }
 
   onSubmit = (): void => {
---
+++ src/app/auth/store/selectors.ts
@@ -0,0 +1,9 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import {AppStateInterface} from "../../shared/types/appState.interface";
+import {AuthStateInterface} from "../types/authState.interface";
+
+export const AuthFeatureSelector = createFeatureSelector<AppStateInterface, AuthStateInterface>('auth');
+
+export const isSubmittingSelector = createSelector(AuthFeatureSelector, (authState: AuthStateInterface) => authState.isSubmitting);
+
+
